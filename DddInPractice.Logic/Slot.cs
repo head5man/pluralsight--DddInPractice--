80b65bebcbcb7cb97b4b6ae5c165bcf374ffad89
Tuomas Lahtinen <tuomas123lahtinen@gmail.com>
2024-12-14 19:31:06 +0200

    Introduce Slots and Snacks

---
+++ DddInPractice.Logic/Slot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DddInPractice.Logic
+{
+    public class Slot : Entity
+    {
+        protected Slot()
+        {
+        }
+
+        public Slot(SnackMachine snackMachine, int position, Snack snack, int quantity, decimal price)
+            : this()
+        {
+            Snack = snack;
+            Quantity = quantity;
+            Price = price;
+            SnackMachine = snackMachine;
+            Position = position;
+        }
+
+        public virtual Snack Snack { get; set; }
+        public virtual int Quantity { get; set; }
+        public virtual decimal Price { get; set; }
+        public virtual SnackMachine SnackMachine { get; protected set; }
+        public virtual int Position { get; protected set; }
+    }
+}
